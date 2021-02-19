0812f2df14874a2b1daddc2c5a2b039b6bdba5da
徐琳杰 <476348131@qq.com>
2021-02-19 17:14:55 +0800

    初步路由搭建完成

---
+++ yky/src/route.js
@@ -0,0 +1,45 @@
+import React from "react";
+import {
+  BrowserRouter as Router,
+  Switch,
+  Route,
+  Link
+} from "react-router-dom";
+import HomePage from "./views/HomePage";
+import Intro from "./views/Intro";
+import Login from "./views/Login";
+
+
+export default function BasicExample() {
+  return (
+    <Router>
+      <div>
+        <ul style={{ display: "flex", flexDirection: "row", width: "500px", justifyContent: "space-between" }}>
+          <li>
+            <Link to="/">登陆注册页面</Link>
+          </li>
+          <li>
+            <Link to="/intro">介绍页面</Link>
+          </li>
+          <li>
+            <Link to="/homePage">首页</Link>
+          </li>
+        </ul>
+
+        <hr />
+
+        <Switch>
+          <Route exact path="/">
+            <HomePage />
+          </Route>
+          <Route path="/intro">
+            <Intro />
+          </Route>
+          <Route path="/homePage">
+            <Login />
+          </Route>
+        </Switch>
+      </div>
+    </Router>
+  );
+}
---
+++ yky/src/views/HomePage/index.jsx
@@ -0,0 +1,12 @@
+import React, { Component } from 'react';
+import s from "./style.module.scss";
+
+export default class Homepage extends Component {
+    render() {
+        return (
+            <div className={s.body}>
+                this is homepage
+            </div>
+        )
+    }
+}
