c44edd4d49b2268c22453eb010a32b5ec3c8bf4d
Rosingsun <476348131@qq.com>
2021-04-22 09:49:57 +0800

    数据展示

--- yky/src/views/Data/index.jsx
+++ yky/src/views/Data/index.jsx
@@ -1,11 +1,35 @@
 import React, { Component } from 'react';
+import * as echarts from 'echarts';
 import s from "./style.module.scss";
 
 export default class Data extends Component {
+  constructor(props){
+    super(props);
+    this.state={
+      data:[5, 20, 36, 10, 10, 20]
+    }
+  }
+  componentDidMount() {
+    var myChart = echarts.init(document.getElementById('main'));
+    myChart.setOption({
+      title: { text: '数据展示' },
+      tooltip: {},
+      xAxis: {
+        data: ["衬衫", "羊毛衫", "雪纺衫", "裤子", "高跟鞋", "袜子"]
+      },
+      yAxis: {},
+      series: [{
+        name: '销量',
+        type: 'bar',
+        data: this.state.data
+      }]
+    });
+  }
   render() {
     return (
       <div className={s.body}>
-        <a>你好 Data</a>
+        <div id="main" style={{ width: 400, height: 400 }}></div>
+        <div id="main" style={{ width: 400, height: 400 }}></div>
       </div>
     )
   }
